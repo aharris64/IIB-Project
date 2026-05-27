e00c3279d25d02a30eb9ac0ed2584c0b7816ec93
aharris64 <adam64harris@gmail.com>
2026-05-27 16:29:29 +0100

    lowres14small

--- cnn/config.py
+++ cnn/config.py
@@ -4,12 +4,12 @@ EXPERIMENT_NOTE = (
 )
 
 # Model
-MODEL_NAME = "mobilenet_v3"
+MODEL_NAME = "mobilenet_v3_small"
 NUM_CLASSES = 3
 FREEZE = "two_phase"   # options: "none" | "head" | "two_phase"
 
 # Data
-DATASET = "disc_centred_r4.0_cl34_augmented_lowres112"
+DATASET = "disc_centred_r4.0_cl34_augmented_lowres14"
 IMAGE_SIZE = 224
 BATCH_SIZE = 32
 
